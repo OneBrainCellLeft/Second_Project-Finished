7249af6ef5822e3468ae13c92c75a6e79a80883b
OBCL <patrickdias3630@gmail.com>
2024-08-27 13:29:08 +0200

    Added a Sign up/Log in page
    Only the buttons work for now and it's pretty ugly but I will add a way to sign up/log in as well as improve a bit the looks of the page

--- Website/app.js
+++ Website/app.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const toTopBtn = document.querySelector('.back__top button');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+toTopBtn.addEventListener("click", function() {
+    window.scrollTo({top: 0, behavior: 'smooth'})
 });
